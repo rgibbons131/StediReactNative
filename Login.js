f305054049d15394bebdb4e8940ea97993588536
Richard Gibbons <richiegibbons99@gmail.com>
2022-06-07 13:41:50 -0600

    added Login

---
+++ Login.js
@@ -0,0 +1,36 @@
+import {useState} from "react";
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+
+const Login = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [oneTimePassword, setOneTimePassword] = useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+        placeholder="123-456-7890"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setOneTimePassword}
+        value={oneTimePassword}
+        placeholder="****"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default UselessTextInput;
